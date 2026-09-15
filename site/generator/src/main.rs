35a3a91b9f2e212ddbda9e23ad59e9a318c4e6ab
Joshua T. Fisher <jofisher@microsoft.com>
2026-09-15 15:43:21 -0700

    Fix the links with the new domain

--- site/generator/src/main.rs
+++ site/generator/src/main.rs
@@ -23,7 +23,7 @@ async fn main() -> anyhow::Result<()> {
     if !args.contains(&"--no-serve".to_owned()) {
         println!("Link to site: http://127.0.0.1:4040/");
 
-        let site = warp::path("").and(warp::fs::dir(config.output_dir));
+        let site = warp::fs::dir(config.output_dir);
 
         warp::serve(site).run(([127, 0, 0, 1], 4040)).await;
     }
